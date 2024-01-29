071f5b98dd18e20c879872ae9472938e66ad669a
f3rn4nd0000 <42982873+f3rn4nd0000@users.noreply.github.com>
2024-01-29 10:58:38 -0300

    saving progress

--- src/_fix_import.py
+++ src/_fix_import.py
@@ -2,5 +2,7 @@ import os
 import sys
 
 current_path = os.path.join(os.path.dirname(sys.path[0]), "")
+parent_path  = os.path.abspath(os.path.join(current_path, os.pardir))
 
 sys.path.append(current_path)
+sys.path.append(parent_path)
---
+++ src/analysis/hypothesis1.py
@@ -0,0 +1,24 @@
+# MOSTRAR QUE A MAIORIA DOS ESTUDANTES USA DISPOSITIVOS MOVEIS
+import _fix_import
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import utils.dataset as dataset
+import utils.filters as filters
+from work import get_most_used_device
+from most_used_device import get_most_used_device
+
+def run():
+    number_of_working_faggots = get_most_used_device
+    print(number_of_working_faggots)
+
+if __name__ == "__main__":
+    run()
+
+""" EXEMPLO DE MAIN
+def run():
+    filtered_answers_study_hours = filters.study_hours(dataset.file)
+    avg_study_hours = get_avg_study_hours(filtered_answers_study_hours)
+    plot_avg_study_hours(avg_study_hours)
+"""
